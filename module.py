b709c6e532075847fd02ab864b91b2f7c305af4f
Jafar8088 <az.jaff880@gmail.com>
2026-08-07 16:43:16 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,13 @@
+# Modules in python
+
+# Create a file named as module_1.py
+# use module_1.py 
+# import module_1
+
+# module_1.say_hello('Jafar') 
+# module_1.say_bye('Sadiq')
+
+from module_1 import person1
+print(person1['age'])
+
+# Package : collection of modules/py files + __init__.py 
---
+++ module_1.py
@@ -0,0 +1,8 @@
+# Create a cpde to use in another py file
+# def say_hello(name):
+    # return print(f"Hello, {name}!, how are you")
+
+# def say_bye(name):
+
+    # return print(f"Hello, {name}!, bye take care")
+person1 = {'name' : 'Keshave', 'age':16 } 
